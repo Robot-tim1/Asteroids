6235b5c0c9afffb9e65ba3e5890c6c8cbb4cf7ff
Robot-tim1 <memopada8@gmail.com>
2026-03-14 15:03:11 +0700

    grouped stuff

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     pygame.time.Clock()
     dt = 0
@@ -20,8 +26,10 @@ def main():
         
         screen.fill("black")
         
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for things in drawable:    
+            things.draw(screen)
 
         pygame.display.flip()
 
