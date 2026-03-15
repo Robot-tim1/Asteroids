8d435a43e299cc9b09aed17b2e666e4cb5b2fcb2
Robot-tim1 <memopada8@gmail.com>
2026-03-15 18:12:58 +0700

    asteroids die now

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from logger import log_state, log_event
 from player import *
 from asteroid import *
 from asteroidfield import AsteroidField
-from shot import Shot
+from shot import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -44,7 +44,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
-        
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         for things in drawable:    
             things.draw(screen)
 
